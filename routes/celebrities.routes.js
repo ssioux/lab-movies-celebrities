8017401f70a3eac21007149a6fa3c51b6a39627a
Curro Garcia <curro-91@hotmail.com>
2022-10-18 18:20:50 +0200

    It 3 e It4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const Celebrities = require("../models/Celebrity.model.js")
 
 // GET "/celebrities/create" => Ruta para renderizar el formulario
 
@@ -12,10 +12,40 @@ router.get("/create", (req, res, next) =>{
 
 router.post("/create", async (req, res, next)=>{
 
-
     console.log(req.body)
+    const { name, occupation, catchPhrase} = req.body
+
+    
+    try {
+        //1. usar la data para crear un nuevo libro
+        await Celebrities.create({
+            name,
+            occupation,
+            catchPhrase        
+        })
+        res.redirect("/celebrities")
+    } catch (error) {
+        res.render("celebrities/new-celebrities.hbs")
+        //!res.redirect("/celebrities/create") CUAL PONER?¿?
+        next(error)
+    }
 
 })
+
+// GET "/celebrities" => Ruta para acceso a la lista
+router.get("/", async (req, res, next) => {
+    try {
+        const celebritiesList = await Celebrities.find()
+        console.log(celebritiesList)
+        res.render("celebrities/celebrities.hbs", {
+            celebritiesList
+        })
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 
 
