f84e098a67ca476965f25d102be00417eab5ff42
David Lazaro <dlazaro7@gmail.com>
2022-10-18 16:21:57 +0200

    Iteration 1 Done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
+const express = require("express");
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+// RUTA de CELEBRITIES
+
+const celebritiesRoutes = require("./celebrities.routes.js");
+router.use("/celebrities", celebritiesRoutes);
+
+// RUTA de MOVIES
+
+const moviesRoutes = require("./movies.routes.js");
+router.use("/movies", moviesRoutes);
 
 module.exports = router;
