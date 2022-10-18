364627eb95ac3bbad25660e0cce86aed892f7ab5
David Lazaro <dlazaro7@gmail.com>
2022-10-18 17:11:38 +0200

    probando

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+const celebritiesSchema = new mongoose.Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+const Celebrities = mongoose.model("Celebrities", celebritiesSchema);
+
+module.exports = Celebrities;
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
 
 
+// GET "/celebrities/create" => Ruta para renderizar el formulario
+
+router.get("/create", (req, res, next) =>{
+
+    res.render("celebrities/new-celebrities.hbs")
+})
+
+// POST "/celebrities/create" => Ruta que crea una nueva celebridad
+
+router.post("/create", async (req, res, next)=>{
 
 
+    console.log(req.body)
+
+})
 
 
 
