54c7bc0c817bd17a3e6a5e79df925549bfc19ad2
Curro Garcia <curro-91@hotmail.com>
2022-10-18 23:51:05 +0200

    lab done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -59,6 +59,78 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+// GET "/movies/:movieID/details" => ruta para renderizar los detalles le los libros
+router.get("/:movieID/details", async(req, res, next) => {
+  console.log("entrando en los detalles de los libros")
+  const {movieID} = req.params
+  
+  try {
+    const details = await Movie.findById(movieID)
+    .populate("cast") // busca la id del objeto de cast(celebrities model)
+    console.log(details)
+    res.render("movies/movie-details.hbs", {
+      details
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+// POST "/movies/:movieID/delete" => ruta para eliminar la film de bd
+router.post("/:movieID/delete", async (req, res, next) => {
+
+  const {movieID} = req.params
+  try {
+    await Movie.findByIdAndDelete(movieID)
+    res.redirect("/movies")
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET "/movies/:movieID/update" => ruta para renderizar un formulario con la data acutal
+router.get("/:movieID/update", async(req, res, next) => {
+  console.log("entrando en update")
+  const {movieID} =req.params
+  try {
+    const detailsUpdate = await Movie.findById(movieID)
+    .populate("cast")
+    const detailsCelebrities = await Celebrities.find()
+    console.log(detailsCelebrities)
+    res.render("movies/edit-movies.hbs", {
+      detailsUpdate,
+      detailsCelebrities
+    })
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
+//POST "movies/:movieID/update" => recibir los valores (action) para actualizar la pelicula y redireccionar
+router.post("/:movieID/update", async(req, res, next) => {
+  console.log("entrando en el post de actualizar la pelicula")
+  const {movieID} = req.params
+  const {title, genre, plot, cast} =req.body
+  
+  console.log(cast)
+  
+  try {
+    const movieUpdate = {
+      title,
+      genre,
+      plot,
+      cast
+    }
+    await Movie.findByIdAndUpdate(movieID, movieUpdate)
+    res.redirect(`/movies/${movieID}/details`)
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
 
 
 
